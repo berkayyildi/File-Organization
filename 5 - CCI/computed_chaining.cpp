fc0501205921b0d5540f6567abb4c154a57e87d0
DESKTOP-LV2ACQP\Berkay <berkay-yildiz@hotmail.com>
2018-11-30 03:59:37 +0300

    CCI %87 Success

--- 5 - CCI/computed_chaining.cpp
+++ 5 - CCI/computed_chaining.cpp
@@ -5,23 +5,38 @@ using namespace std;
 void ComputedChaining::insert(int key){
 
     int moddedData = key % data.size();  // Take mod of the data with table size
-    int quotientData = key / data.size();
 
     if (data[moddedData].valid == false){   //--------------------------------------------------------------------->MODDED BOS ISE ORAYA YERLESTIR
         data[moddedData].key = key;
         data[moddedData].valid = true;
         data[moddedData].plink = -1;
-       cout << "\nNumber " << key << " Placed to index " << moddedData << " (Space Free)" << endl;
+        cout << "\nNumber " << key << " Placed to index " << moddedData << " (Space Free)" << endl;
     }else{                                  //--------------------------------------------------------------------->MODDED DOLU ISE (VALID ISE) PLINK BOS ISE
 
-        if (data[moddedData].plink == -1){  //PLINK BOS ISE
 
-            int quotientOfPlacedNum = data[moddedData].key / data.size();  //Eklenecek olan konumdaki anahtarin arttirim degeri
+        if (data[moddedData].key % data.size() == moddedData)  {//---------------------------------------------------------------------> O YERDE BULUNAN KEY KENDI EVINDEYSE
 
-            //Find free place
-            int freeplace = moddedData;
+            //Find the first number which null offset to place key
+            int quatient = data[moddedData].key / data.size();
+            if (quatient == 0){quatient = 1;}
+
+            int freeplink;
+            if (data[moddedData].plink == -1){  //PLINK BOS ISE
+                    freeplink = moddedData;
+            }else{                              //PLINK DOLU ISE
+                    freeplink = ((data[moddedData].key % data.size()) + data[moddedData].plink * quatient) % data.size();  //modofplacednumber + (plink * arttirimsayisi) sonra mod table size
+            }
+
+            while(data[freeplink].plink != -1){  //Bos plink bulana kadar dÃ¶n
+                freeplink = (freeplink + (data[moddedData].plink * quatient)) % data.size();
+            }
+
+            //Åžimdi PLINK BOS ISE kÄ±smÄ± gibi davran ve sayiyi yerlestir (freeflink i kullan)
+
+            int quotientOfPlacedNum = data[freeplink].key / data.size();  //Eklenecek olan konumdaki anahtarin arttirim degeri
+            int freeplace = freeplink;
             int kacKezDondum = 0;
-            while(data[freeplace].valid == true){   //Boþ yer bulana kadar dön
+            while(data[freeplace].valid == true){   //BoÅŸ yer bulana kadar dÃ¶n
                 freeplace = (freeplace + quotientOfPlacedNum) % data.size();
                 kacKezDondum ++;
             }
@@ -31,146 +46,80 @@ void ComputedChaining::insert(int key){
             data[freeplace].valid = true;
             data[freeplace].plink = -1;
 
-            //Onu gösteren linki set et
+            //Onu gÃ¶steren linki set et
 
-            data[moddedData].plink = kacKezDondum;
+            data[freeplink].plink = kacKezDondum;
 
+            cout << "\nNumber " << key << " Placed to index " << freeplace << " (Own Home)" << endl;
 
-            cout << "\nNumber " << key << " Placed to index " << freeplace << " (Plink Free)" << endl;
+        }else{                          //---------------------------------------------------------------------------------------------> KENDI EVINDE DEGILSE
 
-        }else{                                                      //---------------------------------------------------------------------> PLINK DOLU ISE
+            //Find the index mod and numbers comes after this key (find them with trace plink)
+            vector<int> removed_keys;
+            int removedKeyIndex = moddedData;
+            while(data[removedKeyIndex].plink != -1){  //Bos plink bulana kadar dÃ¶n
 
-
-            if (data[moddedData].key % data.size() == moddedData)  {//---------------------------------------------------------------------> O YERDE BULUNAN KEY KENDI EVINDEYSE
-
-                //Find the null offset to place num
-
-                int KeyinGosterdigiIndex = ((data[moddedData].key % data.size()) + (data[moddedData].plink * data[moddedData].key / data.size())) % data.size();  //modofplacednumber + (plink * arttirimsayisi) sonra mod table size
-
-                //---------------------------------------------------------------------------------------
-
-                int quotientOfPlacedNum = data[KeyinGosterdigiIndex].key / data.size();  //Eklenecek olan konumdaki anahtarin arttirim degeri
-
-                //Find free place
-                int freeplace = KeyinGosterdigiIndex;
-                int kacKezDondum = 0;
-                while(data[freeplace].valid == true){   //Boþ yer bulana kadar dön
-                    freeplace = (freeplace + quotientOfPlacedNum) % data.size();
-                    kacKezDondum ++;
-                }
-
-                //Datayi yerine koy
-                data[freeplace].key = key;
-                data[freeplace].valid = true;
-                data[freeplace].plink = -1;
-
-                //Onu gösteren linki set et
-
-                data[KeyinGosterdigiIndex].plink = kacKezDondum;
-
-                //---------------------------------------------------------------------------------------
-
-                cout << "\nNumber " << key << " Placed to index " << freeplace << " (Own Home -> Plink Full -> Plink Free)" << endl;
-
-
-
-            }else{                          //---------------------------------------------------------------------------------------------> KENDI EVINDE DEGILSE
-
-
-                //Find the index mod and numbers comes after this key (find them with trace plink)
-                vector<int> removed_keys;
-                int removedKeyIndex = moddedData;
-                while(data[removedKeyIndex].plink == -1){  //Bos plink bulana kadar dön
-
-                    cout << "\nNumber " << data[removedKeyIndex].key << " added to removed_keys()" << endl;
-                    removed_keys.push_back(data[removedKeyIndex].key);
-                    data[removedKeyIndex].key = 999;                    //REMOVE NUMBER
-                    data[removedKeyIndex].valid = false;                //REMOVE NUMBER
-                    data[removedKeyIndex].plink = -1;                   //REMOVE NUMBER
-
-
-                    removedKeyIndex =  (removedKeyIndex +(data[removedKeyIndex].plink * data[removedKeyIndex].key / data.size())) % data.size();
-
-                }
-
-                cout << "\nNumber " << data[removedKeyIndex].key << " added to removed_keys()" << endl;
-                removed_keys.push_back(data[removedKeyIndex].key);  //Add last number to the removed_keys here
-                data[removedKeyIndex].key = 999;                    //REMOVE NUMBER
+                cout << "Number " << data[removedKeyIndex].key << " added to removed_keys()" << endl;
+                removed_keys.push_back(data[removedKeyIndex].key);
                 data[removedKeyIndex].valid = false;                //REMOVE NUMBER
+                int removedPlink = data[removedKeyIndex].plink;     //Store Keys Plink
                 data[removedKeyIndex].plink = -1;                   //REMOVE NUMBER
 
+                int quatient = data[removedKeyIndex].key / data.size();
+                if(quatient == 0){quatient = 1;}
 
-                //Sayiyi mod degerine koy (orasi bosaltilmisti)
-
-                data[moddedData].key = key;
-                data[moddedData].valid = true;
-                data[moddedData].plink = -1;
-                cout << "\nNumber " << key << " Placed to index " << moddedData << " (Unplaced Mod)" << endl;
-
-                //Silinen Sayilari Tekrar Yerlestir
-
-                    int rmkey = removed_keys[0];
-
-                    int moddedData_rmkey = rmkey % data.size();  // Take mod of the data with table size
-
-                    int kacKezDondum = 0;
-                    int placement = moddedData_rmkey;
-
-                    while(data[placement].valid == true){   //Boþ yer bulana kadar dön
-                        placement = (placement + data[moddedData_rmkey].key / data.size()) % data.size();   //Silinen keyin modunda bulunan sayýnýn quatienti kadar arttir hep !!!
-                        kacKezDondum ++;
-                    }
-
-                    //Datayi yerine koy
-                    data[placement].key = rmkey;
-                    data[placement].valid = true;
-                    data[placement].plink = -1;
-
-                    //Onu gösteren linki set et
-
-                    data[moddedData_rmkey].plink = kacKezDondum;
-
-                    cout << "\nNumber " << rmkey << " Placed to index " << placement << " (Removed Key Type 1)" << endl;
-
-
-                for (int i=1 ; i < removed_keys.size() ; i++){
-
-
-                    int quotientOfPlacedNum = data[placement].key / data.size();  //Eklenecek olan konumdaki anahtarin arttirim degeri
-
-                    //Find free place
-                    int freeplace = placement;
-                    int kacKezDondum = 0;
-                    while(data[freeplace].valid == true){   //Boþ yer bulana kadar dön
-                        freeplace = (freeplace + quotientOfPlacedNum) % data.size();
-                        kacKezDondum ++;
-                    }
-
-                    //Datayi yerine koy
-                    data[freeplace].key = removed_keys[i];
-                    data[freeplace].valid = true;
-                    data[freeplace].plink = -1;
-
-                    //Onu gösteren linki set et
-
-                    data[placement].plink = kacKezDondum;
-
-
-                    cout << "\nNumber " << removed_keys[i] << " Placed to index " << freeplace << " (Removed Key Type 2)" << endl;
-
-
-                }
+                removedKeyIndex =  (removedKeyIndex +(removedPlink * quatient)) % data.size();
 
             }
 
+            cout << "Number " << data[removedKeyIndex].key << " added to removed_keys()" << endl;
+            removed_keys.push_back(data[removedKeyIndex].key);  //Add last number to the removed_keys here
+            data[removedKeyIndex].key = 999;                    //REMOVE NUMBER
+            data[removedKeyIndex].valid = false;                //REMOVE NUMBER
+            data[removedKeyIndex].plink = -1;                   //REMOVE NUMBER
 
 
+            //Sayiyi mod degerine koy (orasi bosaltilmisti)
 
+            data[moddedData].key = key;
+            data[moddedData].valid = true;
+            data[moddedData].plink = -1;
+            cout << "\nNumber " << key << " Placed to index " << moddedData << " (Unplaced Mod)" << endl;
 
+            //Silinen Sayilari Bos Yerlere Tekrar Yerlestir
+
+            int position = removed_keys[0] % data.size();           //Silinen ilk keyin quatienti
+            int old_position = position;
+
+            for (int i=0 ; i < removed_keys.size() ; i++){
+
+                int startquatient = data[position].key / data.size();
+                int kacKezDondum = 0;
+                position = (position + (kacKezDondum * startquatient)) % data.size();   //ilk seferinde 0 gelicek position = position olacak sonra hep ustune ekleyerek gidicek
+
+                while(data[position].valid == true){   //BoÅŸ yer bulana kadar dÃ¶n
+                    position = (position + startquatient) % data.size();
+                    kacKezDondum ++;
+                    cout << " | " << position;
+                }
+
+                //Datayi yerine koy
+                data[position].key = removed_keys[i];
+                data[position].valid = true;
+                data[position].plink = -1;
+
+                //Onu gÃ¶steren linki set et
+
+                data[old_position].plink = kacKezDondum;
+
+                //Eski datanin linkini koy ve positionu guncelle
+                old_position = position;
+
+                cout << "\nNumber " << removed_keys[i] << " Placed to index " << position << " (Removed Key Placed)" << endl;
+
+            }
 
         }
-
-
     }
 
 }
